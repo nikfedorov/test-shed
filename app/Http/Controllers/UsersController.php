3a170f2ff15de59031680c0447fa57dbf38bfba7
Unknown <neudor@gmail.com>
2019-07-15 19:14:17 +0500

    implement controller action

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,11 +4,29 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
+use Illuminate\Support\Facades\Hash;
 
 class UsersController extends Controller
 {
     public function index()
     {
         return User::all()->toJson();
+    }
+
+    public function create(Request $request)
+    {
+        $validInput = $this->validate($request, [
+            'email' => 'required|email|max:255|unique:users',
+            'name' => 'required|max:255',
+            'password' => 'required|min:8',
+        ]);
+
+        $user =  User::create([
+            'name' => $validInput['name'],
+            'email' => $validInput['email'],
+            'password' => Hash::make($validInput['password']),
+        ]);
+
+        return $user->toJson();
     }
 }
