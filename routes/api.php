e61bf4a0f412f63cbed8b1cb05ba01a71b6e4ec8
Unknown <neudor@gmail.com>
2019-07-15 19:41:29 +0500

    make users.create route more RESTful

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ Route::group(['middleware' => 'api', 'prefix' => 'auth'], function () {
 
 Route::group(['middleware' => ['api', 'auth'], 'prefix' => 'users'], function () {
     Route::get('','UsersController@index')->name('api.users.index');
-    Route::post('create', 'UsersController@create')->name('api.users.create');
+    Route::post('', 'UsersController@create')->name('api.users.create');
     Route::put('{id}', 'UsersController@update')->name('api.users.update');
     Route::delete('{id}', 'UsersController@delete')->name('api.users.delete');
 });
