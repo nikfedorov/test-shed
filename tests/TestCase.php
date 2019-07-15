0f21272f38e47405b1da4f623c7997e39ea6b687
Unknown <neudor@gmail.com>
2019-07-15 18:40:17 +0500

    common authentication for all tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,9 +4,22 @@ namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
     use RefreshDatabase;
+
+    public function authenticate()
+    {
+        $user = factory(User::class)->create([
+            'email' => 'test@gmail.com',
+            'password' => bcrypt('secret1234'),
+        ]);
+
+        $token = JWTAuth::fromUser($user);
+        return $token;
+    }
 }
